04c43d5ab254344ef287b8774d409e57b996bfc0
osm3rr <osm3rr@gmail.com>
2024-11-13 18:14:18 -0400

    delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="home"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("post/new/", PostCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="delete"),
 ]
